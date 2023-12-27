12f02c909485b4127a595eb3d53eb9a38a68dbe0
Artur Papikian <ap@netinsiders.com>
2023-12-27 11:32:42 +0100

    fixed the test

--- tests/module.test.mjs
+++ tests/module.test.mjs
@@ -1,8 +1,8 @@
 import {describe, it, expect} from 'vitest'
-import  '../dist/index.mjs'
+import  Threads from '../dist/index.mjs'
 
 describe('#Functionality', () => {
-    const threads = new  Threads(100)
+    const threads = new Threads(100)
 
     it('Constructing', () => {
         expect(threads.threads.length).toEqual(navigator.hardwareConcurrency - 1 ?? 3)
