61f1c5b46e9b64fdca20db24fe06386f456f9289
Artur Papikian <ap@netinsiders.com>
2024-05-17 17:39:09 +0200

    fixed node worker

--- src/controllers/core/Executor.node.ts
+++ src/controllers/core/Executor.node.ts
@@ -1,38 +1,49 @@
 import {Worker as NodeWorker} from 'worker_threads'
-import {writeFileSync} from 'fs'
+import {writeFileSync, unlinkSync} from 'fs'
 import {tmpdir} from 'os'
 import {join} from 'path'
 import ExecutorInterface, {Command} from '../../types/core/Executor'
 
 
+
 export default class Executor implements ExecutorInterface {
     readonly #worker: NodeWorker
+    readonly #scriptPath: string
 
-    #completedCallback: (message: any) => void = (): void => {
-    }
+    #completedCallback: (message: any) => void = (): void => {}
 
     constructor() {
-        const script = `
-            self.onmessage = async (message) => {
-                const data = message.data
-                switch (data.command) {
+        const script = `   
+            (async function run() {
+                const parentPort = typeof module !== 'undefined' && module.exports 
+                    ? require('worker_threads').parentPort 
+                    : await import('worker_threads').then((worker) => worker.parentPort)
+                 
+                console.log(1)     
+                parentPort.on('message', async (message) => {
+                    console.log(2)               
+                    const data = message.data
+                    switch (data.command) {
                     case 'run':
                         const fn = new Function('return ' + data.task)()
                         const value = await fn(data.value)
-                        postMessage(value)
+                        parentPort.postMessage(value)
                         break
                     case 'terminate':
-                        self.close()
+                        parentPort.close()
                         break
-                }
-            }`
+                    }
+                })
+            })().then()`
 
-        const scriptPath: string = join(tmpdir(), `worker-${Date.now()}.js`)
-        writeFileSync(scriptPath, script)
 
-        this.#worker = new NodeWorker(scriptPath)
+        this.#scriptPath = join(tmpdir(), `worker-${Date.now()}.js`)
+        writeFileSync(this.#scriptPath, script)
+
+        this.#worker = new NodeWorker(this.#scriptPath)
         this.#worker.on('message', (message: any) => this.#completedCallback(message))
         this.#worker.on('error', (err: Error) => {
+            unlinkSync(this.#scriptPath)
             throw new Error('Worker internal error occurred ' + err.message)
         })
 
@@ -50,5 +61,6 @@ export default class Executor implements ExecutorInterface {
 
     terminate(): void {
         this.#worker.postMessage({command: Command.TERMINATE})
+        unlinkSync(this.#scriptPath)
     }
 }
