f194004cf233c9bc8c7fa1e7c9bf5729a9284ac6
Artur Papikian <ap@netinsiders.com>
2024-05-17 15:13:45 +0200

    fix merge unresolved

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import Threads from './controllers/core/Threads'
 import TaskPool from './controllers/partials/TaskPool'
-import LiveConnector from './controllers/partials/LiveConnector'
-
-
-const liveConnector: LiveConnector = new LiveConnector()
 
 export default Threads
-export {TaskPool, liveConnector}
+export {TaskPool}
 
