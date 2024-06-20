502e89c04d21b86bea7b1e881063b0bebe7ebcb9
Artur Papikian <ap@netinsiders.com>
2024-06-20 14:20:46 +0200

    test modified + queue

---
+++ src/controllers/core/utils/Queue.ts
@@ -0,0 +1,59 @@
+export default class Queue {
+
+    #queue: number[] = []
+
+    get length(): number {
+        return this.#queue.length
+    }
+
+    push(value: number): void {
+        this.#queue.push(value)
+
+        this.#sort()
+    }
+
+    at(index: number): number | undefined {
+        return this.#queue.at(index)
+    }
+
+    pushIncrement(from?: number): number {
+        if (from) {
+            this.#queue.push(from)
+            return from
+        }
+
+        if (this.#queue.length === 0) {
+            this.#queue.push(0)
+            return 0
+        }
+
+        const value: number = this.at(- 1)! + 1
+        this.#queue.push(value)
+
+        return value
+    }
+
+    #sort(): void {
+        this.#queue.sort((a: number, b: number): number => a - b)
+    }
+
+    last(): number | undefined {
+        return this.at(-1)
+    }
+
+    splice(index: number, length: number = 1): void {
+        this.#queue.splice(index, length)
+    }
+
+    spliceByValue(value: number, length: number = 1): void {
+        const index: number = this.#queue.indexOf(value)
+        if (index === -1) return
+
+        this.#queue.splice(index, length)
+    }
+
+    spectate(): number[] {
+        return this.#queue.slice()
+    }
+
+}
